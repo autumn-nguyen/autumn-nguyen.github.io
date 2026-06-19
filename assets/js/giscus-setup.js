d44a81e861adb6eeab9d98fd9414f9646258e61c
autumn-nguyen <92401509+autumn-nguyen@users.noreply.github.com>
2026-06-19 08:21:12 +0000

    Deploying to gh-pages from @ autumn-nguyen/autumn-nguyen.github.io@cea3e0331cf636e2bf0be651602277a904de3443 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "autumn-nguyen/al-folio",
+    "data-repo": "autumn-nguyen/autumn-nguyen.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
