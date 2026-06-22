f5bb2da4f87cace0f56ba2aae5edcbe449e8bd8d
autumn-nguyen <92401509+autumn-nguyen@users.noreply.github.com>
2026-06-22 07:51:25 +0000

    Deploying to gh-pages from @ autumn-nguyen/autumn-nguyen.github.io@8e70313aa79ee3a3770d82c54367de4cb48a94f0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-more",
+  },{id: "nav-updates",
+          title: "updates",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/updates/";
+          },
+        },{id: "nav-more",
           title: "more",
           description: "",
           section: "Navigation",
